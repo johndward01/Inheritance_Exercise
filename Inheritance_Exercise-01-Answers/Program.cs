8eb3802faf1ea3ce13655eec38abbcf80af5433c
johndward01 <johndward01@gmail.com>
2021-08-12 21:42:22 -0500

    added TODO regions on answer

--- Inheritance_Exercise-01-Answers/Program.cs
+++ Inheritance_Exercise-01-Answers/Program.cs
@@ -4,9 +4,31 @@ namespace Inheritance_Exercise_01_Answers
 {
     class Program
     {
+        #region// TODO: Demonstrate inheritance using a class
+        // TODO: Create a base class called Dog
+        // TODO: 
+
+        // TODO: Create a Beagle class that inherits from the Dog class
+        // TODO: Give 
+
+        #endregion
+
+        #region// TODO: Demonstrate Inheritance using an abstract class
+
+
+
+
+        #endregion
+
+
+        #region// TODO: Demonstrate Multiple Inheritance using Interfaces
+
+
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: 
         }
     }
 }
